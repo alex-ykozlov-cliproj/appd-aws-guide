05092a2ca39eb3c1eaffaef429e5f37a7018f2bd
alex kozlov <ykozlov@gmail.com>
2021-09-15 12:18:27 -0400

    Fix env vars for appd db agent

--- db-agent/build-image.sh
+++ db-agent/build-image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export IMAGE_NAME=ghcr.io/alex-ykozlov-cliproj/appd-aws-ingestion/appd-dbagent:1.0.0
+export IMAGE_NAME=ghcr.io/alex-ykozlov-cliproj/appd-aws-ingestion/appd-dbagent:21.2.0
 
 
 docker build -t ${IMAGE_NAME} -f Dockerfile ./src
